3f9815d62e05e63cc4c4532bd1d1d9a4c153084c
Kapsteroo <kasperczykkacper@gmail.com>
2023-05-03 16:37:23 +0200

    Font update

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -12,10 +12,10 @@ export class DataService {
   ];
 
   private lokalizacja = {
-    adres: 'ul. Testowa 1, 00-000 Warszawa',
+    adres: 'Henryka Sienkiewicza 12, 43-603 Jaworzno',
     trasa:
       'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet justo donec enim. In metus vulputate eu scelerisque felis imperdiet proin fermentum.',
-    telefon: '+48 123 456 789',
+    telefon: '+48 572 512 733',
     godzinyOtwarcia: 'Poniedziałek - piątek 10:00-20:00',
     email: 'przykladowa@przychodnia.pl',
   };
